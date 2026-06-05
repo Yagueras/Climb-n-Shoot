11991f5cd2047de1c149e3e73b438ff0143e502c
JS1331 <jansarda2003@gmail.com>
2026-06-05 18:39:11 +0200

    IDamagable/ITriggerChackable + canvis necessaris

--- Assets/Scripts/DimorphodonManager.cs
+++ Assets/Scripts/DimorphodonManager.cs
@@ -1,15 +1,24 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit.AffordanceSystem.Receiver.Primitives;
 
-public class DimorphodonManager : MonoBehaviour
+public class DimorphodonManager : MonoBehaviour, IDamageable, ITriggerCheckable
 {
+    protected int maxHealth = 10;
+    protected int currentHealth;
     private float speed;
     private Transform player;
     public AudioClip raptorSound;
     public AudioSource audioSource;
+    public int scoreGiven = 10;
+    public SpriteRenderer spriteRenderer;
+    public Color hitColor = new Color(1f, 0.3f, 0.3f);
+    private bool IsWithinStrikingRange = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        currentHealth = maxHealth;
         speed = 3f;
         player = GameObject.FindGameObjectWithTag("Player").transform;
         audioSource.PlayOneShot(raptorSound);
@@ -30,5 +39,38 @@ public class DimorphodonManager : MonoBehaviour
             //logica VIDA player
             Destroy(gameObject);
         }
+    }
+
+    public void Damage(int damage)
+    {
+        currentHealth -= damage;
+
+        StartCoroutine(FlashRed());
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        //GameManager.instance.AddScore(scoreGiven);
+
+        //FindObjectOfType<EnemySpawner>().EnemyDied();
+
+        Destroy(gameObject);
+    }
+
+    public void SetStrikingDistance(bool isWithinStrikingRange)
+    {
+        IsWithinStrikingRange = isWithinStrikingRange;
+    }
+
+    System.Collections.IEnumerator FlashRed()
+    {
+        spriteRenderer.color = hitColor;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.color = Color.white;
     }
 }
