336145cc74793677f2242f80ac03926c394d1731
Yagueras <yago.jsbs@gmail.com>
2026-06-06 20:30:39 +0200

    State machine removed added overheating and cooldown mechanic

--- Assets/Scripts/Gun/Base/Gun.cs
+++ Assets/Scripts/Gun/Base/Gun.cs
@@ -1,66 +1,47 @@
 using System;
-using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit.Locomotion.Comfort;
+using UnityEngine.UI;
 
 public class Gun : MonoBehaviour
 {
     #region Gun Statistics
-
     [Header("Stats")]
-
     private float minHeatCapacity = 0f;
     [SerializeField] public float maxHeatCapacity = 20f;
     [SerializeField] public float bulletHeat = 2f;
     private float currentGunTemperature = 0f;
-
     #endregion
 
     #region Overheat timer variables
     [Header("Overheating")]
-
     [SerializeField] public float overheatCooldownDuration = 5f;
-    [SerializeField] public float coolDownStartTimer = 0.5f;
+    [SerializeField] public float gunCoolDownStartTimer = 1f; // Time to wait before cooling starts
     [SerializeField] public float coolDownSpeed = 3f;
     private bool overheated = false;
 
     [SerializeField] public float attackCooldownDuration = 0.2f;
     private float _currentCooldownTimer;
-
     private bool _attackCoolingDown;
+
+    // NEW: Dedicated timer to track how long since the last shot was fired
+    private float _timeSinceLastShot;
     #endregion
 
-    #region State Machine Variables
+    [SerializeField] public Slider gunHeatSlider;
 
-    public GunStateMachine StateMachine { get; set; }
-    public GunCoolState CoolState { get; set; }
-    public GunShootingState ShootingState { get; set; }
-    public GunCoolingDownState CoolingDownState { get; set; }
-    public GunOverheatedState OverheatedState { get; set; }
-
-    #endregion
+    [SerializeField] public ParticleSystem confetti;
 
     private void Awake()
     {
-        StateMachine = new GunStateMachine();
-
-        CoolState = new GunCoolState(this, StateMachine);
-        ShootingState = new GunShootingState(this, StateMachine);
-        CoolingDownState = new GunCoolingDownState(this, StateMachine);
-        OverheatedState = new GunOverheatedState(this, StateMachine);
+        _currentCooldownTimer = attackCooldownDuration;
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        StateMachine.Initialize(CoolState);
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        StateMachine.CurrentGunState.FrameUpdate();
+        AttackCooldown();
+        StartCoolingDownWeapon();
 
+        UpdateHeatSlider();
     }
 
     public void Shoot()
@@ -68,39 +49,84 @@ public class Gun : MonoBehaviour
         if (overheated)
             return;
 
-        if (_attackCoolingDown)
+        if (!_attackCoolingDown)
         {
-            _currentCooldownTimer += Time.deltaTime;
-
-            if (_currentCooldownTimer >= attackCooldownDuration)
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector3.forward, out hit))
             {
-                RaycastHit hit;
-
-                if (Physics.Raycast(transform.position, Vector3.forward, out hit))
-                Debug.Log("Found an object - distance: " + hit.distance);
-                ApplyHeat(bulletHeat);
-
+                // Raycast logic
             }
+
+            ApplyHeat(bulletHeat);
+            confetti.Play();
+
+            // Reset attack cooldown
+            _currentCooldownTimer = 0f;
+            _attackCoolingDown = true;
+
+            // NEW: Reset the shot timer so cooling pauses when you shoot
+            _timeSinceLastShot = 0f;
+
+            Debug.Log($"Shot fired! Current Temp: {currentGunTemperature}");
         }
-        
-
-        
-
-        //TODO: SoundEffects (KaboomKablawKaboom)
     }
 
     private void ApplyHeat(float heat)
     {
         currentGunTemperature += heat;
-        currentGunTemperature = Math.Clamp(heat, minHeatCapacity, maxHeatCapacity);
+        currentGunTemperature = Mathf.Clamp(currentGunTemperature, minHeatCapacity, maxHeatCapacity);
+
         if (currentGunTemperature >= maxHeatCapacity)
         {
             overheated = true;
+            Debug.Log("Weapon Overheated!");
         }
     }
 
-    private void StartWeaponCooling()
+    private void AttackCooldown()
     {
+        if (_currentCooldownTimer < attackCooldownDuration)
+        {
+            _currentCooldownTimer += Time.deltaTime;
+        }
+        else
+        {
+            _attackCoolingDown = false;
+        }
+    }
 
+    private void StartCoolingDownWeapon()
+    {
+        // Track how long it has been since we last shot
+        _timeSinceLastShot += Time.deltaTime;
+
+        float currentCooldownTime = gunCoolDownStartTimer;
+        if (overheated)
+            currentCooldownTime = overheatCooldownDuration;
+
+        // If we haven't waited long enough, don't cool down yet
+        if (_timeSinceLastShot <= currentCooldownTime)
+            return;
+
+        // Cool down using your coolDownSpeed variable, clamped so it doesn't go below min
+        if (currentGunTemperature > minHeatCapacity)
+        {
+            currentGunTemperature -= coolDownSpeed * Time.deltaTime;
+            currentGunTemperature = Mathf.Clamp(currentGunTemperature, minHeatCapacity, maxHeatCapacity);
+
+            Debug.Log("Reducing temps: " + currentGunTemperature);
+
+            // Optional: If you want the gun to become usable again after cooling down completely
+            if (overheated && currentGunTemperature <= minHeatCapacity)
+            {
+                overheated = false;
+                Debug.Log("Weapon cooled down. Ready to fire!");
+            }
+        }
+    }
+
+    private void UpdateHeatSlider()
+    {
+        gunHeatSlider.value = currentGunTemperature;
     }
 }
